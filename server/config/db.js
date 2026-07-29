ae4beaa5f283a2b6be891ce01168e4bdeaac5ffb
Baraa <ahmedbaraa009@gmail.com>
2026-07-29 15:58:28 +0300

    Vercel deploy

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDb = () =>
   mongoose
-    .connect(process.env.DATABASE_LOCAL)
+    .connect(process.env.DATABASE_ATLAS)
     .then((conn) =>
       console.log(
         `database connected successfully on host: ${conn.connection.host}`,
